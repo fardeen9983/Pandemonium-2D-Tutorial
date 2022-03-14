7c0ddb10aec0ce6c37d515ff67a1ae89b0a74de7
Fardeen Khan <khanfardeen9983@gmail.com>
2022-03-14 23:54:57 +0530

    Added Flip motion

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,54 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /// <summary>
+    /// Script Input
+    /// </summary>
     [SerializeField]
     private float Speed = 10;
 
     [SerializeField]
     private GameObject Ground;
 
+    /// <summary>
+    /// Components
+    /// </summary>
     private Rigidbody2D body;
     private BoxCollider2D selfCollider;
-    
+    private SpriteRenderer spriteRenderer;
+
+    private bool IsFacingLeft = false;
+    private float HorizontalInput;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-        
         selfCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, body.velocity.y);
+        HorizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(HorizontalInput * Speed, body.velocity.y);
+
+        MonitorFlip();
 
         if (Input.GetKeyDown(KeyCode.Space) && selfCollider.IsTouching(Ground.GetComponent<BoxCollider2D>()))
         {
-            body.velocity = new Vector2(body.velocity.x,Speed);
+            body.velocity = new Vector2(body.velocity.x, Speed);
+        }
+    }
+
+    void MonitorFlip()
+    {
+        
+        if ((HorizontalInput < -0.01f && !IsFacingLeft) || (HorizontalInput > 0.01f && IsFacingLeft))
+        {
+            Vector2 scale = transform.localScale;
+            scale.x *=  -1;
+            transform.localScale = scale;
+            IsFacingLeft = !IsFacingLeft;
         }
     }
 }
