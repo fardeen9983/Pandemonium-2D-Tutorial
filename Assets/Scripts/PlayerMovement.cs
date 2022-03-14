c6ed463a298111be43bf76769ea240823124df67
Fardeen Khan <khanfardeen9983@gmail.com>
2022-03-15 00:59:52 +0530

    Jump Animation added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float Speed = 10;
 
-    [SerializeField]
-    private GameObject Ground;
-
     /// <summary>
     /// Components
     /// </summary>
@@ -22,6 +19,8 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsFacingLeft = false;
     private float HorizontalInput;
+    private bool CanJump;
+    private bool Grounded;
 
     private void Awake()
     {
@@ -35,29 +34,50 @@ public class PlayerMovement : MonoBehaviour
         HorizontalInput = Input.GetAxis("Horizontal");
 
         body.velocity = new Vector2(HorizontalInput * Speed, body.velocity.y);
-        
+
         MonitorFlip();
 
-        if (Input.GetKeyDown(KeyCode.Space) && selfCollider.IsTouching(Ground.GetComponent<BoxCollider2D>()))
-        {
-            body.velocity = new Vector2(body.velocity.x, Speed);
+        if (Input.GetKeyDown(KeyCode.Space) && CanJump)
+        { 
+            Jump(); 
         }
 
-
         animator.SetBool("run", HorizontalInput != 0);
-        Debug.Log("Input " + HorizontalInput + ", Running : " + animator.GetBool("run"));
+        animator.SetBool("grounded", Grounded);
 
     }
 
-    void MonitorFlip()
+    private void MonitorFlip()
     {
-
         if ((HorizontalInput < -0.01f && !IsFacingLeft) || (HorizontalInput > 0.01f && IsFacingLeft))
         {
             Vector2 scale = transform.localScale;
             scale.x *= -1;
             transform.localScale = scale;
             IsFacingLeft = !IsFacingLeft;
+        }
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, Speed);
+        Grounded = false;
+        animator.SetTrigger("jump");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Grounded = true;
+            CanJump = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            CanJump = false;
         }
     }
 }
